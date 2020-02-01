386fd31d00d1bbe072c28413918af812efa56845
Marissa Salcido <salcidom@uci.edu>
2020-02-01 14:03:59 -0800

    Added CategoryActivity: When each macro button is pushed, RecyclerView shows list of specified type of food

--- app/src/main/java/com/example/ecocart/CategoryActivity.java
+++ app/src/main/java/com/example/ecocart/CategoryActivity.java
@@ -4,15 +4,29 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoryActivity extends AppCompatActivity {
-
+    RecyclerView rvFoods;
+    List<Food> foods;
+    FoodAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.category);
         String foodType = getIntent().getStringExtra("foodType");
         Log.i("TAG", "CategoryActivity onCreate: "+foodType);
+
+        rvFoods = findViewById(R.id.rvfood);
+        foods = new ArrayList<>();
+        adapter = new FoodAdapter(foods);
+        LinearLayoutManager layoutManager = new LinearLayoutManager((this));
+        rvFoods.setLayoutManager(layoutManager);
+        rvFoods.setAdapter(adapter);
 
     }
 
