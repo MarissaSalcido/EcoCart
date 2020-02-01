2431e4468579b7168966ac1e35e588d535a0fb9e
Shireen Hsu <shireen0718@gmail.com>
2020-02-01 10:55:39 -0800

    add activity_main.xml

--- app/src/main/java/com/example/ecocart/MainActivity.java
+++ app/src/main/java/com/example/ecocart/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_page);
+        setContentView(R.layout.activity_main);
 
     }
 }
