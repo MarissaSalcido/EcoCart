c0ba3b6c2d35637ea29bd02e6ddf4bd2ce316fec
Sharon <sharon.kim971@gmail.com>
2020-01-31 23:36:19 -0800

    added key

---
+++ app/src/main/java/com/example/ecocart/Foods.java
@@ -0,0 +1,4 @@
+package com.example.ecocart;
+
+public class Foods {
+}
--- app/src/main/java/com/example/ecocart/MainActivity.java
+++ app/src/main/java/com/example/ecocart/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     public static final String hi = "hi";
+    String hello;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
