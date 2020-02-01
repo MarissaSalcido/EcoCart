1ca30f1b743c018ca8f3b4277bf4862baba9ab39
Sharon <sharon.kim971@gmail.com>
2020-01-31 21:15:13 -0800

    added key
    ^

--- app/src/main/java/com/example/ecocart/EaternityClient.java
+++ app/src/main/java/com/example/ecocart/EaternityClient.java
@@ -1,5 +1,5 @@
 package com.example.ecocart;
 
 public class EaternityClient {
-    public static final String KEY = "";
+    public static final String KEY = "aDRjSzR0SDBOT2c3NUhqZkszMzlLbE9scGEzOWZKenhYdw==";
 }
